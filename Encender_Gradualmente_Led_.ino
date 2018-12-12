c124153e280c97d1db94c0d1d5804e7dc2082c4d
victorcubedo7 <45770234+victorcubedo7@users.noreply.github.com>
2018-12-12 16:11:34 +0100

    Add files via upload

---
+++ Encender_Gradualmente_Led_.ino
@@ -0,0 +1,25 @@
+int led = 3;  //¡¡¡Es necesario colocar el led en este pin!!!
+
+int brillo;
+
+
+void setup() {
+
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+
+ for ( brillo = 0; brillo < 256; brillo++){
+    analogWrite(led,brillo);
+    delay(20);
+ }
+
+   for ( brillo = 255; brillo >=0; brillo--){
+    analogWrite(led,brillo);
+    delay(20);
+ }
+
+
+
+}
